0912d4cc1db4e6695c54f67e44346a12c62de644
lipkij <lipkij@gmail.com>
2022-12-13 16:51:48 +0400

    draft: add overlay menu and some css styles

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", onLoadFunction);
+document.getElementById('menu_list__wrapper').addEventListener('click', hideOverlay)
 
 function onLoadFunction(e) {
     onResizeFunction();
     window.addEventListener("resize", onResizeFunction);
 }
 
-function redrawMenuButton(){
+function redrawMenuButton() {
     const btnMenu = document.getElementById('menu__button')
     if (screen.width < 400) {
         btnMenu.innerText = "#";
@@ -19,3 +20,18 @@ function redrawMenuButton(){
 function onResizeFunction(e) {
     redrawMenuButton();
 }
+
+function toggleMenu(e) {
+    const menuWrapper = document.getElementById('menu_list__wrapper')
+    if (menuWrapper.style.display == "none" || menuWrapper.style.display == '') {
+        menuWrapper.style.display = "block";
+    } else {
+        menuWrapper.style.display = "none";
+    }
+}
+
+function hideOverlay(e) {
+    const menuWrapper = document.getElementById('menu_list__wrapper')
+    menuWrapper.style.display = "none";
+}
+
